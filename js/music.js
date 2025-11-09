5064a768f71e5f94476c669adfe0ac37f5b0c065
beckham-new <laskarmahadewa@gmail.com>
2025-11-10 01:43:41 +0800

    Delete js/music.js

--- js/music.js
+++
@@ -1,8 +0,0 @@
-// js/music.js
-if (!window.globalMusic) {
-  const music = new Audio("music/softlove.mp3");
-  music.loop = true;
-  music.volume = 0.6;
-  music.play();
-  window.globalMusic = music;
-}
